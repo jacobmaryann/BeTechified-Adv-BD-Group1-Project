9c3549711c9714897f12eec4ba9085f8c0f2c203
Mary Ukamaka Jacob <maryann89492@gmail.com>
2026-09-13 19:05:57 +0100

    initial commit

--- Controller/Notes.controller.js
+++ Controller/Notes.controller.js
@@ -1,3 +1,21 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 const NotesModel = require('../Models/NoteModel.js');
+
+
+const createNote = async (req, res) => {
+};
+
+const getAllNotes = async (req, res) => {
+};
+
+const getNoteById = async (req, res) => {
+};
+
+const updateNoteById = async (req, res) => {
+};
+
+const deleteNoteById = async (req, res) => {
+};
+
+module.exports = { createNote, getAllNotes, getNoteById, updateNoteById, deleteNoteById };
