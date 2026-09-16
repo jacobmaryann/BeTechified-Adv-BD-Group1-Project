a5105ba96e76fd18292af0f506bde031b4d2ce5d
Mary Ukamaka Jacob <maryann89492@gmail.com>
2026-09-16 22:56:56 +0100

    create, get, auth, ownership done

--- App.js
+++ App.js
@@ -3,15 +3,26 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT
-const DBconnect = require("./Database/Dbconnect.js")
+const DBconnect = require("./Database/Dbconnect.js");
+const errorHandler = require('./Middlewares/ErrorHandler.js');
+const logRequests = require('./Middlewares/Logger.js');
 
+
+const NoteRoute = require('./Routes/Note.route.js');
+const UserRoute = require('./Routes/User.route.js');
 
 DBconnect()
 
 
 app.use(express.json());
+app.use('/api', NoteRoute);
+app.use('/api/users', UserRoute);
+app.use(errorHandler);
+app.use(logRequests);
 
-
+app.get('/', (req, res) => {
+    res.status(200).json("Note API on point")
+});
 
 app.listen(PORT, () => {
     console.log (`Server is listening on PORT ${PORT}`)
--- Routes/Note.route.js
+++ Routes/Note.route.js
@@ -1,3 +1,18 @@
 const express = require('express');
-
+const { createNote, getAllNotes, 
+    getNoteById, updateNoteById, 
+    deleteNoteById } = require('../Controller/Notes.controller.js');
 const router = express.Router();
+const requireAuth = require('../Middlewares/requireAuth.js')
+
+router.post('/notes', requireAuth, createNote);
+
+router.get('/notes', requireAuth, getAllNotes);
+
+router.get('/notes/:id', requireAuth, getNoteById);
+
+router.put('/notes/:id', requireAuth, updateNoteById);
+
+router.delete('/notes/:id', requireAuth, deleteNoteById);
+
+module.exports = router;
